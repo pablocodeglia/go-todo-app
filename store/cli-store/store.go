94999c840bf40a70fe7ba4db736377996e391631
pablocodeglia <4017664+pablocodeglia@users.noreply.github.com>
2025-01-23 10:58:13 +0000

    refactors and parallel/benchmark tests

--- store/cli-store/store.go
+++ store/cli-store/store.go
@@ -20,7 +20,6 @@ type TodoStore struct {
 	CurrentUserId string
 	Data          map[string]types.Todo
 	Mu            sync.Mutex
-	Stdin         io.Reader
 }
 
 func NewStore() *TodoStore {
@@ -31,7 +30,6 @@ func NewStore() *TodoStore {
 
 func (s *TodoStore) Add(newTaskName string) {
 
-	cli.Clr()
 	taskUuid := uuid.New()
 
 	s.Mu.Lock()
@@ -46,36 +44,26 @@ func (s *TodoStore) Add(newTaskName string) {
 	println("\n- Success! Task added.\n")
 }
 
-func (s *TodoStore) Delete() {
-	
-	taskId := cli.GetUserInput("Todo's ID to be removed:", s.Stdin)
+func (s *TodoStore) Delete(todoId string) {
 	s.Mu.Lock()
-	delete(s.Data, taskId)
+	delete(s.Data, todoId)
 	s.Mu.Unlock()
-	println("\n- Success! Todo removed.\n")
 
-	s.ListTodos()
-	s.DisplayOptions()
 }
 
-func (s *TodoStore) MarkAsDone() {
-	cli.Clr()
-	s.ListTodos()
-	taskId := cli.GetUserInput("\n- Todo's ID to mark as done:", s.Stdin)
-
+func (s *TodoStore) MarkAsDone(todoId string) {
 	s.Mu.Lock()
 
-	if entry, ok := s.Data[taskId]; ok {
+	if entry, ok := s.Data[todoId]; ok {
 		entry.IsDone = true
-		s.Data[taskId] = entry
+		s.Data[todoId] = entry
 	}
 
 	s.Mu.Unlock()
 
 	cli.Clr()
 	fmt.Printf("\n- Success! Todo marked as done.\n")
-	s.ListTodos()
-	s.DisplayOptions()
+
 }
 
 func (s *TodoStore) ListTodos() {
@@ -96,7 +84,7 @@ func (s *TodoStore) ListTodos() {
 		i++
 	}
 	fmt.Println("")
-	s.Mu.Lock()
+	s.Mu.Unlock()
 
 }
 
@@ -108,12 +96,10 @@ func (s *TodoStore) ClearCache() {
 	s.Mu.Unlock()
 }
 
-func (s *TodoStore) LogUser() {
-	s.CurrentUserId = cli.GetUserInput("Username: ", s.Stdin)
+func (s *TodoStore) LogUser(userId string) {
+	s.CurrentUserId = userId
 	s.ClearCache()
 	s.LoadData()
-	s.ListTodos()
-	s.DisplayOptions()
 }
 
 func (s *TodoStore) LoadData() {
@@ -161,9 +147,8 @@ func (s *TodoStore) SaveChangesToFile() {
 	for k, v := range s.Data {
 		dataTosave.Data = append(dataTosave.Data, map[string]types.Todo{k: v})
 	}
-	
-	s.Mu.Unlock()
 
+	s.Mu.Unlock()
 
 	jsonBytes, err := json.MarshalIndent(dataTosave, " ", " ")
 	if err != nil {
@@ -182,11 +167,6 @@ func (s *TodoStore) SaveChangesToFile() {
 	}
 
 	defer res.Body.Close()
-
-	cli.Clr()
-	fmt.Printf("\n- Success! Changes saved!\n")
-	s.ListTodos()
-	s.DisplayOptions()
 }
 
 func (s *TodoStore) DisplayOptions() {
@@ -196,27 +176,42 @@ func (s *TodoStore) DisplayOptions() {
 	fmt.Printf("3. Delete TODO\n")
 	fmt.Printf("4. Save changes to file\n")
 	fmt.Printf("5. Change current user\n")
-	choice := cli.GetUserInput("\nSelect one option by number:", s.Stdin)
+
+	choice := cli.GetUserInput("\nSelect one option by number:")
 	choiceInt, err := strconv.Atoi(choice)
 	for (err != nil) || !(choiceInt < 6 && choiceInt > 0) {
 		fmt.Printf("Invalid option!!!\n")
 		s.DisplayOptions()
 		break
 	}
+
+	cli.Clr()
+
 	switch choiceInt {
 	case 1:
-		cli.Clr()
-		newTaskName := cli.GetUserInput("New task description: ", s.Stdin)
+		newTaskName := cli.GetUserInput("New task description: ")
 		s.Add(newTaskName)
-		s.ListTodos()
-		s.DisplayOptions()
+
 	case 2:
-		s.MarkAsDone()
+		s.ListTodos()
+		taskId := cli.GetUserInput("\n- Todo's ID to mark as done:")
+
+		s.MarkAsDone(taskId)
+
 	case 3:
-		s.Delete()
+		s.ListTodos()
+		todoId := cli.GetUserInput("Todo's ID to be removed:")
+		s.Delete(todoId)
+		println("\n- Success! Todo removed.\n")
+
 	case 4:
 		s.SaveChangesToFile()
+		fmt.Printf("\n- Success! Changes saved!\n")
+
 	case 5:
-		s.LogUser()
+		userId := cli.GetUserInput("Username: ")
+		s.LogUser(userId)
 	}
+	s.ListTodos()
+	s.DisplayOptions()
 }
--- store/cli-store/store_test.go
+++ store/cli-store/store_test.go
@@ -1,32 +1,40 @@
 package clistore
 
 import (
-	"fmt"
-	"net/http"
+	"reflect"
 	"testing"
-	apiv1 "todoapp/api/v1"
-	httpV1 "todoapp/http"
 )
 
-func initializeApi() {
-	mux := http.NewServeMux()
-	apiv1.RegisterApiHandlers(mux)
-	httpV1.RegisterHttpHandlers(mux)
-	go http.ListenAndServe("localhost:8080", mux)
+func TestAddParallel(t *testing.T) {
+	tests := []struct {
+		name            string
+		todoDescription string
+	}{
+		{"test1", "1st new TODO"},
+		{"test2", "2nd new TODO"},
+		{"test3", "3rd new TODO"},
+		{"test4", "4th new TODO"},
+		{"test5", "5th new TODO"},
+		{"test6", "6th new TODO"},
+	}
+
+	store := NewStore()
+
+	t.Parallel()
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			store.Add(tt.todoDescription)
+			got := len(store.Data)
+			want := i+1
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("expected %d, got %d", want, got)
+			}
+		})
+	}
 }
 
 func TestAdd(t *testing.T) {
-	//initialize server for Api endpoints
-	initializeApi()
-
-	//
 	store := NewStore()
-	store.CurrentUserId = "testuser"
-	// log user
-	store.ClearCache()
-	go store.LoadData()
-
-	fmt.Println(store.CurrentUserId)
 
 	store.Add("TEST NEW TASK")
 	store.Add("TEST ANOTHER NEW TASK")
